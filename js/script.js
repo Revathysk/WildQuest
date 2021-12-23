9fb8a7643d8fb408c9cab009d99a97c107fe12af
Revathy <revathyskandasamy@gmail.com>
2021-12-22 20:23:51 -0600

    confetti time interval

--- js/script.js
+++ js/script.js
@@ -119,21 +119,15 @@ function checkMasterQ(player) {
         //confetti code
         //inlcude confetti.js code in script.js
         //document.writeln("<script type='text/javascript' src='js/confetti.js'></script>");
-        const start = () => {
-            setTimeout(function() {
-                confetti.start()
-            }, 500); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
-        };
+
+        setTimeout(function() {
+            confetti.start()
+        }, 1000); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
 
         //  for stopping the confetti 
-        const stop = () => {
-            setTimeout(function() {
-                confetti.stop()
-            }, 2000); // 5000 is time that after 5 second stop the confetti ( 5000 = 5 sec)
-        };
-        // after this here we are calling both the function so it works
-        confetti.start();
-        confetti.stopt();
+         setTimeout(function() {
+            confetti.stop()
+        }, 5000); // 5000 is time that after 5 second stop the confetti ( 5000 = 5 sec)
     }
     else {
         answer.style.backgroundColor = 'yellow';
