0c06f31192849a08d88461a6c6367f2700a90536
Revathy <revathyskandasamy@gmail.com>
2021-12-22 21:47:53 -0600

    Added imagesfrom JS to display on grid

--- js/script.js
+++ js/script.js
@@ -6,63 +6,75 @@ const answer = document.querySelector('#textBox');
 let p1Count = 0;
 let p2Count = 0;
 let counter = 0;
-let winQcount = 11;
-let counterMax = 9;
+let winQcount = 12;
+let counterMax = 11;
 let index= -1;
 let gridSelect='';
 const quiz = [
 	{
 		question: "Name the eagle that feeds on monkey and sloth",
-		correctAns: 'harpy'
-
+		correctAns: 'harpy',
+        image: 'https://gifts.worldwildlife.org/gift-center/Images/large-species-photo/large-Harpy-Eagle-photo.jpg'
 	},
 	{
 		question: "Animal with 4 stomach, has red nose",
-		correctAns: 'reindeer' 		
+		correctAns: 'reindeer',
+        image: 'https://www.greenpeace.org/static/planet4-canada-stateless/2018/06/140074_252367.jpg'	
 	},
     {
 		question: "Animal with unique finger print as human",
-		correctAns: 'koala' 		
+		correctAns: 'koala',
+        image:'https://cdn.britannica.com/26/162626-050-3534626F/Koala.jpg'
 	},
     {
 		question: "Sealife with heart in head",
-		correctAns: 'shrimp' 		
+		correctAns: 'shrimp',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMs5322MVDreww0an28ZGRV_lLMnmGO7r5Lml26whln5BzzjSQKRrFO0DwjFmfs3OR6dY&usqp=CAU' 		
 	},
     {
 		question: "Sealife with no heart,brain and bone",
-		correctAns: 'jellyfish' 		
+		correctAns: 'jellyfish',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUEU4ja1V7ulgWCTySoWoI0l8T8J6xs2g-A4_39VUranMb_FlhmnTcPBlfuwmsp3KfyQk&usqp=CAU'		
 	},
     {
-		question: " myhorn is made of hair...",
-		correctAns: 'rhino' 
+		question: " Has horn on top of nose and horn is made of hair...",
+		correctAns: 'rhino',
+        image: 'https://files.worldwildlife.org/wwfcmsprod/images/White_Rhino/story_full_width/15g9slqvru_white_rhino_42993643.jpg'
     },
     {
 		question: "Lives in desert, can drink 25 gallons of water at a time",
-		correctAns: 'camel' 
+		correctAns: 'camel',
+        image: 'https://www.thoughtco.com/thmb/FS0_P5QLMgc-xWU4kH7lWALWLUE=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/camel1-5c8fc2b1c9e77c0001eb1c6f.jpg'
     },
     {
-		question: "Know to have spines,as many as 30,000",
-		correctAns: 'porcupine' 
+		question: "Know to have lot of spines,as many as 30,000",
+		correctAns: 'porcupine',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/PorcupineCabelasSpringfield0511.jpg/220px-PorcupineCabelasSpringfield0511.jpg' 
     },
     {
 		question: "Digs netwrok of chambers and tunnels called sett",
-		correctAns: 'badger' 
+		correctAns: 'badger',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Badger_laying_on_ground._-_DPLA_-_0335977b4d1504edc799834081ca4fd5.jpg/800px-Badger_laying_on_ground._-_DPLA_-_0335977b4d1504edc799834081ca4fd5.jpg'
     },
     {
 		question: "I'm slow in moving...can sleep for 3 years continuously",
-		correctAns: 'snail' 
+		correctAns: 'snail',
+        image: 'https://cf.ltkcdn.net/kids/images/std/239664-800x493r1-snail.webp'
     },
     {
         question :'An average African elephant weighs about 12,000 pounds. Type in true or false',
-        correctAns:'true'
+        correctAns:'true' ,
+        image: 'https://images.news18.com/ibnlive/uploads/2021/08/jumbo.jpg'
     },
     {
         question : 'Male animal that gives birth to offsprings',                
-        correctAns :'seahorse'
+        correctAns :'seahorse',
+        image : 'https://animalfactguide.com/wp-content/uploads/2021/01/sea_horse_is_horsey-e1609821832721.jpg'
     },
     {
-        question : 'I built not just dam but lodge too with orange teeth',                
-        correctAns :'beavers'
+        question : 'Rodent builds not just dam but lodge too with orange teeth',                
+        correctAns :'beaver',
+        image : 'https://static.dw.com/image/18461438_6.jpg'
     }
 ];    
 
@@ -101,7 +113,7 @@ function gameDisplay(){
 }
 
 function winningQuest(player) {
-    document.querySelector('#masterqDiv').backgroundColor= 'blue';
+    // document.querySelector('#masterqDiv').backgroundColor= 'blue';
     document.querySelector('#masterqDiv').innerText = quiz[winQcount].question; 
     setTimeout(() => {
        checkMasterQ(player);    
@@ -116,10 +128,6 @@ function checkMasterQ(player) {
         answer.style.backgroundColor = 'green';
         document.querySelector('#masterqDiv').innerText=`Congrats! ${player},you won. Press Start, to try again`;
         
-        //confetti code
-        //inlcude confetti.js code in script.js
-        //document.writeln("<script type='text/javascript' src='js/confetti.js'></script>");
-
         setTimeout(function() {
             confetti.start()
         }, 1000); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
@@ -142,17 +150,18 @@ function checkAnswer()
     // let imageSelect = '#image'+ counter;
     console.log (getinput);
 
-    if (getinput === quiz[counter].correctAns && counter%2 === 0) {
-        p1Count++;
-        console.log('player 1 gets a point');        
-        document.querySelector(gridSelect).innerText=p1Name.innerText;
-        // document.querySelector('#image1').classList.toggle('hide');       
-    }
-    else if (getinput === quiz[counter].correctAns && counter%2 === 1) {
-        p2Count++;
-        document.querySelector(gridSelect).innerText=p2Name.innerText;
-        console.log('player 2 gets a point');
-        // document.querySelector(`#image2`).classList.toggle('hide');
+    if (getinput === quiz[counter].correctAns) {
+        
+        if (counter%2 === 0){
+            p1Count++;    
+        } else {    
+            p2Count++;
+        }      
+        document.querySelector(gridSelect).innerText='';
+        let crtimage = document.createElement('img');
+        crtimage.setAttribute("style", "width: 12vw; height: 16vh; -webkit-border-radius: 15px;");
+        crtimage.src = quiz[counter].image;
+        document.querySelector(gridSelect).appendChild(crtimage);
     }
     else {
         answer.style.backgroundColor = 'red';
@@ -199,27 +208,25 @@ function playerName()
      // Reset counter for players
      if(p1Count>0 || p2Count>0){
          let input=prompt("Do you want to reset game? Type 'y' or 'n'");
-         if( input === 'y')
-         {
-                p1Count=0;
-                p2Count=0;
-                counter=0;
-                index=-1;
-                p1Name.innerText= "";
-                p2Name.innerText= "";
-                document.querySelector('#masterqDiv').innerText="";
-                answer.value = "";
-                for (let i=1;i<=9;i++){
-                    console.log(`.grid${i}`);
-                    document.querySelector(`.grid${i}`).innerText= '';
-                }
-         
+        if( input === 'y'){
+            p2Count=0;
+            p1Count=0;
+            counter=0;
+            p1Name.innerText= "";
+            index=-1;
+            p2Name.innerText= "";
+            document.querySelector('#masterqDiv').innerText="";
+            answer.value = "";
+            answer.style.backgroundColor = 'white';
+            for (let i=1;i<=9;i++){
+                console.log(`.grid${i}`);
+                document.querySelector(`.grid${i}`).innerText= '';
+            }
         }
      }
 
     let input= prompt("Enter First player's name:")
     p1Name.innerText=input;
     input= prompt("Enter Second player's name:")
-    p2Name.innerText=input;
-        
+    p2Name.innerText=input;        
 }
